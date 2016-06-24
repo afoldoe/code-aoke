be5622f224f13b06e843bc235d11ecbe845c53c6
Arielle Foldoe <afoldoe@gmail.com>
2016-06-23 18:29:59 -0700

    separated nav functionality

--- scripts/landingController.js
+++ scripts/landingController.js
@@ -6,6 +6,19 @@
       }, 2000);
   };
 
+  controller.navHandler = function() {
+    $(window).scroll(function() {
+      if($(window).scrollTop() < 100) {
+        $('nav').addClass('scrollDown');
+        $('nav').removeClass('scrollUp');
+      } else {
+        $('nav').removeClass('scrollDown');
+        $('nav').addClass('scrollUp');
+      }
+    });
+
+  };
+
   $('#startButton').on('click', controller.gameStartController);
   $('#gameStartNav').on('click', controller.gameStartController);
   module.controller = controller;
