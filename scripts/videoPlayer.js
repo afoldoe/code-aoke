5b3ae42106f32602f2a0fbb2e52f4c6615eddcaa
cassburt <cass.burt@gmail.com>
2016-06-24 20:58:59 -0700

    video player size altered. VP fullscreen button added. Autoplay disabled. Message added. VP player section added in index git push origin cassB23 git status

--- scripts/videoPlayer.js
+++ scripts/videoPlayer.js
@@ -7,6 +7,13 @@
        height: '390',
        width: '640',
        videoId: id,
+       playerVars: {
+               autohide: 1,
+               controls: 0,
+               showinfo: 0,
+               disablekb: 1,
+               modestbranding: 1
+           },
        events: {
          'onReady': onPlayerReady
         //  'onStateChange': onPlayerStateChange
@@ -15,22 +22,31 @@
    }
 
   videoPlayer.Ready = function(){
-  $('#video-list').on('click', '.video-image', function(){
-    $('#video-list').empty()
-    var id = $(this).parent('div , .video-details').attr('id');
-    // var id = $(this).parent('.video-details').id
-    // console.log(id);
-    onYouTubeIframeAPIReady(id);
-   })
+    $('#video-list').on('click', '.video-image', function(){
+      $('#video-list').empty();
+      var id = $(this).parent('div , .video-details').attr('id');
+      onYouTubeIframeAPIReady(id);
+      $('#fullscreen').append("<button id='fullview'>Play Fullscreen</button>");
+      $('#message').append("<h1 id='salute'>For Those About to Rock, We Salute You!</h1>");
+    })
   }
 
- function onPlayerReady(event) {
-     event.target.playVideo();
+ function onPlayerReady(event){
+      player = event.target;
+      iframe = document.querySelector('#player');
    }
 
-   videoPlayer.initAll = function(){
-     videoPlayer.Ready();
-   }
+  $("#fullscreen").on("click", function(){
+    player.playVideo();//won't work on mobile
+    var requestFullScreen = iframe.requestFullScreen || iframe.mozRequestFullScreen || iframe.webkitRequestFullScreen;
+    if (requestFullScreen) {
+      requestFullScreen.bind(iframe)();
+    }
+  })
+
+  videoPlayer.initAll = function(){
+    videoPlayer.Ready();
+  }
 
   module.videoPlayer = videoPlayer;
 
